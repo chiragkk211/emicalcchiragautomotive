f7d111ba913c12d5de2e536e42e4363d9e1b1f25
chiragkk211 <83634102+chiragkk211@users.noreply.github.com>
2024-01-31 15:29:44 +0530

    Add files via upload
    refreshed kratos r also added

---
+++ index.js
@@ -0,0 +1,40 @@
+
+function calculateEMI(){
+
+    var modelS = document.getElementById("model");
+    var model = modelS.value;
+    var DP = parseInt(document.getElementById("downpayment").value)
+    var tenureM = parseInt(document.getElementById("tenure").value)
+    var vehiclePrice=0
+
+    if (model=="Magnus EX"){
+      vehiclePrice=113760
+    }
+    if(model=="Zeal EX"){
+      vehiclePrice=100500
+    }
+    if(model=="Primus"){
+        vehiclePrice=137335
+      }
+      if(model=="Kratos R"){
+        vehiclePrice=182700
+      }
+    
+    margin=DP-2000
+    LA=(vehiclePrice+1500-margin)/0.98
+    var intrt=(10.99/12)/100
+   
+    
+    intpm=LA*intrt
+    installments=Math.round((LA+(intpm*tenureM))/tenureM)
+    totVehPrice=(installments*tenureM)+DP
+    totInterest=totVehPrice-vehiclePrice
+  
+
+    document.getElementById("vehprice").innerText= vehiclePrice
+    document.getElementById("installments").innerText= installments
+    document.getElementById("price").innerText= totVehPrice
+    document.getElementById("interest").innerText= totInterest
+  
+
+}
